159b0ca2f3d16d74e94b166e3491f16f3d2eeb50
Angel Avelar-Bonilla <angelavelarbonilla@gmail.com>
2025-11-09 15:58:43 -0500

    Add gitignore, and add functions to recursively require Class files

--- main.lua
+++ main.lua
@@ -1,5 +1,12 @@
-function love.load()
+Class = require 'libraries.classic.classic'
+Test = require 'objects.Test'
 
+function love.load()
+  image = love.graphics.newImage('resources/cat.jpg')
+  love.window.setMode(800, 600, {resizable=true, vsync=0})
+  local object_files = {}
+  recursiveEnumerate('objects', object_files)
+  requireFiles(object_files)
 end
 
 function love.update(dt)
@@ -7,5 +14,28 @@ function love.update(dt)
 end
 
 function love.draw()
-
+  love.graphics.draw(image, love.math.random(0,800), love.math.random(0,600), 0, 0.5, 0.5)
 end
+
+-- Recursively enumerate all files in given folder
+-- Then adds them as strings to a table
+function recursiveEnumerate(folder, file_list)
+  local items = love.filesystem.getDirectoryItems(folder)
+  for _, item in ipairs(items) do
+    local file = folder .. '/' .. item
+    if love.filesystem.isFile(file) then
+      table.insert(file_list, file)
+    elseif love.filesystem.isDirectory(file) then
+      recursiveEnumerate(file, file_list)
+    end
+  end
+end
+
+function requireFiles(files)
+  for _, file in ipairs(files) do
+    local file = file:sub(1,-5)
+    require(file)
+  end
+end
+
+
